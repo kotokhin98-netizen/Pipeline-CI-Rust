3f649cf234381aaa45564549ad735192818212d1
art3m <kotokhin98@list.ru>
2026-05-11 20:51:13 +0300

    add

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Hello from Rust in Docker!");
+    io::stdout().flush().unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(200));
+}
